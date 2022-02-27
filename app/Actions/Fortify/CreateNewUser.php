bf99ec361aab830be74fd936bb97a3e2793fccd1
Muhammad Thalenta Dirgantara Deha <mthalentadeha@gmail.com>
2022-02-27 21:32:52 +0800

    Menambahkan commit file

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array  $input
+     * @return \App\Models\User
+     */
+    public function create(array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => $this->passwordRules(),
+            'address' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+            'age' => ['required', 'string', 'max:255'],
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        return User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+            'address' => $input['address'],
+            'phone' => $input['phone'],
+            'age' => $input['age'],
+        ]);
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('ABOUT') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h2>WEBSITE LEARNING MIDAS</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quo voluptatibus vero repellendus dignissimos excepturi? Eveniet soluta, quaerat repellat numquam sunt reprehenderit illum, assumenda sed quasi facilis, ut enim molestiae.</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
